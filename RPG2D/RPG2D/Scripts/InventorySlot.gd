c71f88895fb8415986499a14055eb4ceba980eaa
Nabiz <dawcio.gwizdz@gmail.com>
2021-10-23 23:13:36 +0200

    drag item system

--- RPG2D/RPG2D/Scripts/InventorySlot.gd
+++ RPG2D/RPG2D/Scripts/InventorySlot.gd
@@ -3,16 +3,28 @@ extends Panel
 var is_mouse_hold = false
 var item_scene = preload("res://Scenes/Items/Item.tscn")
 var tile_size = 64
+var null_id = 12
 
 func _on_InventorySlot_gui_input(event):
     if event.is_action_pressed("ui_left_mouse"):
-        is_mouse_hold = true
+        if $Item.id != null_id:
+            is_mouse_hold = true
 
 func _input(event):
     if is_mouse_hold and event.is_action_released("ui_left_mouse"):
         is_mouse_hold = false
         var new_item = item_scene.instance()
         new_item.set_id($Item.id)
-        new_item.position = (get_global_mouse_position()-Vector2.ONE * tile_size/2).snapped(Vector2.ONE * tile_size) + Vector2.ONE * tile_size/2
         get_tree().root.get_child(0).add_child(new_item)
-        $Item.set_id(0)
+        new_item.position = (get_global_mouse_position()-Vector2.ONE * tile_size/2).snapped(Vector2.ONE * tile_size) + Vector2.ONE * tile_size/2
+        new_item.is_recently_draged = true
+        yield(get_tree().create_timer(0.1), "timeout")
+        new_item.is_recently_draged = false
+        $Item.set_id(null_id)
+
+func _on_Item_area_entered(area):
+    if area.is_recently_draged and $Item.id == null_id:
+        area.is_recently_draged = false
+        $Item.set_id(area.id)
+        yield(get_tree().create_timer(0.1), "timeout")
+        area.queue_free()
--- RPG2D/RPG2D/Scripts/Item.gd
+++ RPG2D/RPG2D/Scripts/Item.gd
@@ -5,6 +5,8 @@ var id = 128
 var is_mouse_hold = false
 var player = null
 
+var is_recently_draged = false
+
 func _ready():
     $Sprite.frame = id
     position = position.snapped(Vector2.ONE * tile_size/2)
@@ -24,5 +26,7 @@ func _input(event):
         is_mouse_hold = false
         if player:
             if position.distance_to(player.position) < 1.42 * tile_size:
-                var new_position = (get_global_mouse_position()-Vector2.ONE * tile_size/2).snapped(Vector2.ONE * tile_size)
-                position = new_position + Vector2.ONE * tile_size/2
+                global_position = (get_global_mouse_position()-Vector2.ONE * tile_size/2).snapped(Vector2.ONE * tile_size) + Vector2.ONE * tile_size/2
+                is_recently_draged = true
+                yield(get_tree().create_timer(0.1), "timeout")
+                is_recently_draged = false
